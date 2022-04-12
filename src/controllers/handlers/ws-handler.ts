1bca18e122ddb99e4b294663a2b7f5a8835e6a66
Juan Scolari <jscolari@decentraland.org>
2022-04-12 17:31:06 -0300

    Revert "feat: implement heeartbeat"
    This reverts commit cbcd4192e62ac26896e65cc82ad89cee5bc1652d.

--- src/controllers/handlers/ws-handler.ts
+++ src/controllers/handlers/ws-handler.ts
@@ -6,16 +6,12 @@ import * as proto from "../proto/broker"
 import { Category, DataHeader, PositionData, ProfileData } from "../proto/comms"
 import { Subscription } from "nats"
 
-const HEARTBEAT_REFRESH_RATE = 100 // ms
 const connections = new Set<WebSocket>()
 const subscriptionsPerConnection = new WeakMap<WebSocket, Set<Subscription>>()
-
-const connectionHeartbeats = new WeakMap<WebSocket, number>()
 
 let connectionCounter = 0
 
 const aliasToUserId = new Map<number, string>()
-type Position3D = [number, number, number]
 
 function getSubscriptionsList(socket: WebSocket): Set<Subscription> {
   let set = subscriptionsPerConnection.get(socket)
@@ -25,7 +21,6 @@ function getSubscriptionsList(socket: WebSocket): Set<Subscription> {
   }
   return set
 }
-
 export async function websocketHandler(context: IHttpServerComponent.DefaultContext<GlobalContext>) {
   const messageBroker = context.components.messageBroker
   const logger = context.components.logs.getLogger("Websocket Handler")
@@ -62,25 +57,6 @@ export async function websocketHandler(context: IHttpServerComponent.DefaultCont
         const topicMessage = proto.TopicMessage.deserializeBinary(data)
 
         const topic = topicMessage.getTopic()
-
-        // Heartbeat
-        const body = topicMessage.getBody() as any
-        const dataHeader = DataHeader.deserializeBinary(body)
-        const category = dataHeader.getCategory()
-        if (category === Category.POSITION) {
-          const positionData = PositionData.deserializeBinary(body)
-          const lastHearbeat = connectionHeartbeats.get(ws)
-          const now = Date.now()
-          if (!lastHearbeat || lastHearbeat < now - HEARTBEAT_REFRESH_RATE) {
-            connectionHeartbeats.set(ws, now)
-            const position = positionData.toObject()
-            const peerPositionChange = {
-              id: aliasToUserId.get(alias),
-              position: [position.positionX, position.positionY, position.positionZ],
-            }
-            messageBroker.publish("heartbeat", peerPositionChange)
-          }
-        }
 
         const topicFwMessage = new proto.TopicFWMessage()
         topicFwMessage.setType(proto.MessageType.TOPIC_FW)
