a3dca1f917e9bf9f3d20b288f6dfa471e0893525
Juan Scolari <jscolari@decentraland.org>
2022-04-18 13:52:11 -0300

    feat: create bff websocket endpoint

---
+++ src/controllers/handlers/ws-bff-handler.ts
@@ -0,0 +1,64 @@
+import { upgradeWebSocketResponse } from "@well-known-components/http-server/dist/ws"
+import { IHttpServerComponent } from "@well-known-components/interfaces"
+import { WebSocket } from "ws"
+import { GlobalContext } from "../../types"
+import { HeartBeatMessage, MessageHeader, MessageType, MessageTypeMap } from "../proto/bff_pb"
+import { WorldPositionData } from "../proto/comms_pb"
+
+const connections = new Set<WebSocket>()
+
+let connectionCounter = 0
+
+const aliasToUserId = new Map<number, string>()
+
+export async function websocketBFFHandler(context: IHttpServerComponent.DefaultContext<GlobalContext>) {
+  const logger = context.components.logs.getLogger("Websocket BFF Handler")
+  logger.info("Websocket")
+
+  return upgradeWebSocketResponse((socket) => {
+    logger.info("Websocket connected")
+    // TODO fix ws types
+    const ws = socket as any as WebSocket
+
+    connections.add(ws)
+
+    const alias = ++connectionCounter
+
+    const query = context.url.searchParams
+    const userId = query.get("identity") as string
+    aliasToUserId.set(alias, userId)
+
+    ws.on("message", (message) => {
+      const data = message as Buffer
+      let msgType = MessageType.UNKNOWN_MESSAGE_TYPE as MessageTypeMap[keyof MessageTypeMap]
+      try {
+        msgType = MessageHeader.deserializeBinary(data).getType()
+      } catch (err) {
+        logger.error("cannot deserialize message header")
+        return
+      }
+
+      switch (msgType) {
+        case MessageType.UNKNOWN_MESSAGE_TYPE: {
+          logger.log("unsupported message")
+          break
+        }
+        default: {
+          logger.log(`ignoring msgType ${msgType}`)
+          break
+        }
+      }
+    })
+
+    ws.on("error", (error) => {
+      logger.error(error)
+      ws.close()
+      connections.delete(ws)
+    })
+
+    ws.on("close", () => {
+      logger.info("Websocket closed")
+      connections.delete(ws)
+    })
+  })
+}
--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -3,6 +3,7 @@ import { GlobalContext } from "../types"
 import { pingHandler } from "./handlers/ping-handler"
 import { statusHandler } from "./handlers/status-handler"
 import { websocketHandler } from "./handlers/ws-handler"
+import { websocketBFFHandler } from "./handlers/ws-bff-handler"
 import { websocketRoomHandler } from "./handlers/ws-room-handler"
 
 // We return the entire router because it will be easier to test than a whole server
@@ -13,6 +14,7 @@ export async function setupRouter(globalContext: GlobalContext): Promise<Router<
 
   router.get("/status", statusHandler)
   router.get("/ws", websocketHandler)
+  router.get("/ws-bff", websocketBFFHandler)
   router.get("/ws-rooms/:roomId", websocketRoomHandler)
 
   return router
