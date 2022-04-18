3b8b28f0a68f57a1337246635b6f54a6fd05e5a3
Juan Scolari <jscolari@decentraland.org>
2022-04-18 18:25:18 -0300

    feat: implement island changes

--- src/controllers/handlers/ws-bff-handler.ts
+++ src/controllers/handlers/ws-bff-handler.ts
@@ -2,8 +2,9 @@ import { upgradeWebSocketResponse } from "@well-known-components/http-server/dis
 import { IHttpServerComponent } from "@well-known-components/interfaces"
 import { WebSocket } from "ws"
 import { GlobalContext } from "../../types"
-import { HeartBeatMessage, MessageHeader, MessageType, MessageTypeMap } from "../proto/bff_pb"
+import { HeartBeatMessage, IslandChangesMessage, MessageHeader, MessageType, MessageTypeMap } from "../proto/bff_pb"
 import { WorldPositionData } from "../proto/comms_pb"
+import { JSONCodec } from "nats"
 
 const connections = new Set<WebSocket>()
 
@@ -28,6 +29,30 @@ export async function websocketBFFHandler(context: IHttpServerComponent.DefaultC
     const query = context.url.searchParams
     const userId = query.get("identity") as string
     aliasToUserId.set(alias, userId)
+
+    // Island Changes
+    // TODO implement types
+    // TODO implement disconnect
+    const islandChangesSubscription = messageBroker.subscribe("island_changes", (data: any) => {
+      const jsonCodec = JSONCodec()
+      const islandChanges = jsonCodec.decode(data) as any
+      Object.keys(islandChanges).forEach((islandChangePeerId) => {
+        const userId = aliasToUserId.get(alias)
+        if (userId !== islandChangePeerId) {
+          return
+        }
+        const message = islandChanges[userId]
+        if (message.action === "changeTo") {
+          const islandId = message.islandId
+          logger.info(`Peer ${userId} moved to island ${islandId}`)
+          const msg = new IslandChangesMessage()
+          msg.setType(MessageType.ISLAND_CHANGES)
+          msg.setTransport("pubsub")
+          msg.setTopic(islandId)
+          ws.send(msg.serializeBinary())
+        }
+      })
+    })
 
     ws.on("message", (message) => {
       const data = message as Buffer
@@ -74,11 +99,13 @@ export async function websocketBFFHandler(context: IHttpServerComponent.DefaultC
     ws.on("error", (error) => {
       logger.error(error)
       ws.close()
+      islandChangesSubscription.unsubscribe()
       connections.delete(ws)
     })
 
     ws.on("close", () => {
       logger.info("Websocket closed")
+      islandChangesSubscription.unsubscribe()
       connections.delete(ws)
     })
   })
