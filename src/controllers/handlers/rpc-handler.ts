40143ef424f2333e4f57635c5c387e6d4aa70b24
Hugo Arregui <hugo@decentraland.org>
2022-06-13 20:55:13 -0300

    chore: add very basic ping keepalive for socket transport (#86)
    Chore: add very basic ping keepalive for socket transport

--- src/controllers/handlers/rpc-handler.ts
+++ src/controllers/handlers/rpc-handler.ts
@@ -2,13 +2,36 @@ import { upgradeWebSocketResponse } from '@well-known-components/http-server/dis
 import { IHttpServerComponent } from '@well-known-components/interfaces'
 import { GlobalContext } from '../../types'
 import { WebSocketTransport } from '@dcl/rpc/dist/transports/WebSocket'
+import { WebSocket } from 'ws'
 
 /**
  * This function attachs a WebSocket handler to the HTTP server
  * to upgrade the connection for the rpcServer
  */
 export async function websocketRpcHandler(context: IHttpServerComponent.DefaultContext<GlobalContext>) {
+  const { logs } = context.components
+  const logger = logs.getLogger('ws')
   return upgradeWebSocketResponse((socket) => {
+    let isAlive = true
+    const ws = socket as any as WebSocket
+    ws.on('pong', () => {
+      isAlive = true
+    })
+
+    const pingInterval = setInterval(function ping() {
+      if (isAlive === false) {
+        logger.warn(`Terminating ws because of ping timeout`)
+        return ws.terminate()
+      }
+
+      isAlive = false
+      ws.ping()
+    }, 30000)
+
+    ws.on('close', () => {
+      clearInterval(pingInterval)
+    })
+
     // first we create an RPC WebSocketTransport
     const transport = WebSocketTransport(socket)
     // then we attach the transport to the rpcServer that will handle it
