0f84e038eb22108a1bd8aa9e40df66ca07ac1cba
Juan Scolari <jscolari@decentraland.org>
2022-06-16 14:35:53 -0300

    feat: implement config endpoint with comms protocol

--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -6,6 +6,7 @@ import { statusHandler } from './handlers/status-handler'
 import { websocketHandler } from './handlers/ws-handler'
 import { websocketRoomHandler } from './handlers/ws-room-handler'
 import { websocketRpcHandler } from './handlers/rpc-handler'
+import { explorerConfigurationHandler } from './handlers/explorer-configuration-handler'
 
 // We return the entire router because it will be easier to test than a whole server
 export async function setupRouter(_: GlobalContext): Promise<Router<GlobalContext>> {
@@ -14,6 +15,7 @@ export async function setupRouter(_: GlobalContext): Promise<Router<GlobalContex
   router.get('/ping', pingHandler)
 
   router.get('/cluster-status', clusterStatusHandler)
+  router.get('/explorer-configuration', explorerConfigurationHandler)
   router.get('/status', statusHandler)
   router.get('/ws', websocketHandler)
   router.get('/rpc', websocketRpcHandler)
