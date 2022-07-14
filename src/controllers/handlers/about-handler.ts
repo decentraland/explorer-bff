6d5f6373c8de6f3772ca3f4b7ce0bcb9605d1817
Hugo Arregui <hugo@decentraland.org>
2022-07-14 11:39:21 -0300

    chore: replace cluster status and explorer configuration with about handler

---
+++ src/controllers/handlers/about-handler.ts
@@ -0,0 +1,80 @@
+import { HandlerContextWithPath } from '../../types'
+
+export type About = {
+  healthy: boolean
+  configurations: {
+    commsProtocol: string
+  }
+  content: {
+    healthy: boolean
+  }
+  comms: {
+    healthy: boolean
+  }
+  lambdas: {
+    healthy: boolean
+  }
+}
+
+// handlers arguments only type what they need, to make unit testing easier
+export async function aboutHandler(
+  context: Pick<
+    HandlerContextWithPath<'serviceDiscovery' | 'logs' | 'metrics' | 'config' | 'fetch', '/explorer-configuration'>,
+    'url' | 'components'
+  >
+) {
+  const { logs, config, fetch, serviceDiscovery } = context.components
+
+  const logger = logs.getLogger('explorer-configuration')
+  const commsProtocol = await config.requireString('COMMS_PROTOCOL')
+  const lambdasUrl = await config.requireString('LAMBDAS_URL')
+
+  const body: About = {
+    healthy: false,
+    configurations: {
+      commsProtocol
+    },
+    content: {
+      healthy: false
+    },
+    comms: {
+      healthy: false
+    },
+    lambdas: {
+      healthy: false
+    }
+  }
+
+  try {
+    const response = await fetch.fetch(`${lambdasUrl}/health`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json'
+      }
+    })
+
+    const data: Record<string, string> = await response.json()
+
+    body.content.healthy = data['content'] === 'Healthy'
+    body.lambdas.healthy = data['lambda'] === 'Healthy'
+    body.comms.healthy = data['comms'] === 'Healthy'
+  } catch (err: any) {
+    logger.error(err)
+  }
+
+  if (commsProtocol === 'v3') {
+    const clusterStatus = await serviceDiscovery.getClusterStatus()
+    if (clusterStatus.archipelago) {
+      body.comms = {
+        healthy: true,
+        ...clusterStatus.archipelago
+      }
+    }
+  }
+
+  body.healthy = body.content.healthy && body.lambdas.healthy && body.comms.healthy
+
+  return {
+    body
+  }
+}
--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -1,11 +1,10 @@
 import { Router } from '@well-known-components/http-server'
 import { GlobalContext } from '../types'
 import { pingHandler } from './handlers/ping-handler'
-import { clusterStatusHandler } from './handlers/cluster-status-handler'
 import { statusHandler } from './handlers/status-handler'
 import { websocketHandler } from './handlers/ws-handler'
 import { websocketRpcHandler } from './handlers/rpc-handler'
-import { explorerConfigurationHandler } from './handlers/explorer-configuration-handler'
+import { aboutHandler } from './handlers/about-handler'
 
 // We return the entire router because it will be easier to test than a whole server
 export async function setupRouter(_: GlobalContext): Promise<Router<GlobalContext>> {
@@ -13,11 +12,10 @@ export async function setupRouter(_: GlobalContext): Promise<Router<GlobalContex
 
   router.get('/ping', pingHandler)
 
-  router.get('/cluster-status', clusterStatusHandler)
-  router.get('/explorer-configuration', explorerConfigurationHandler)
   router.get('/status', statusHandler)
   router.get('/ws', websocketHandler)
   router.get('/rpc', websocketRpcHandler)
+  router.get('/about', aboutHandler)
 
   return router
 }
