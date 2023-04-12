fd2de46caca7534a41acbd1d6fa088b7b84fb3ee
Hugo Arregui <hugo@decentraland.org>
2023-04-12 11:34:36 -0300

    feat: archipelago v2

--- src/controllers/handlers/about-handler.ts
+++ src/controllers/handlers/about-handler.ts
@@ -23,16 +23,16 @@ export async function aboutHandler(
   const maxUsers = await config.getNumber('MAX_USERS')
   const networkId = networkIds[ethNetwork]
 
-  const comms = await context.components.comms.getStatus()
-
-  const [lambdasHealth, contentStatus, lambdasStatus, resourcesOverload] = await Promise.all([
+  const [lambdasHealth, contentStatus, lambdasStatus, resourcesOverload, comms, realmName] = await Promise.all([
     status.getLambdasHealth(),
     status.getContentStatus(),
     status.getLambdasStatus(),
-    resourcesStatusCheck.areResourcesOverloaded()
+    resourcesStatusCheck.areResourcesOverloaded(),
+    context.components.comms.getStatus(),
+    realm.getName()
   ])
 
-  const realmName = await realm.getName()
+  console.log(comms)
 
   const healthy = lambdasHealth.lambdas && lambdasHealth.content && comms.healthy
   const userCount = rpcSessions.sessions.size
